658b50430e2cf917a5e292592b09003a927a72f9
swallow0311 <774207663@qq.com>
2026-06-10 14:11:42 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bold-capybara-run">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<ProductManagement />} />
